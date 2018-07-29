c893f66659d4a5d80a4127373b9fa8fbf2da6bdf
ST\MScully <michael.scully@sterlingts.com>
2018-07-29 19:07:47 -0400

    Added github links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Shoes.com UI Test';
+  constructor(private httpService: HttpClient) { };
+  musicData: string [];
+
+  ngOnInit() {
+    this.httpService.get('../assets/data/UITestData.json').subscribe(
+      data => {
+        this.musicData = data as string [];
+        console.log(this.musicData);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+  }
 }
